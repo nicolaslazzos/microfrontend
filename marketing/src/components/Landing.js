f4381915963d090d035ad7d664cafe7d6918d460
nicolaslazzos <nicolaslazzos09@gmail.com>
2022-08-13 01:58:59 -0300

    change to force the github action

--- marketing/src/components/Landing.js
+++ marketing/src/components/Landing.js
@@ -16,7 +16,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <MaterialLink component={Link} to="/" color="inherit">
-        Your Website
+        Nicolas Lazzos
       </MaterialLink>{' '}
       {new Date().getFullYear()}
       {'.'}
